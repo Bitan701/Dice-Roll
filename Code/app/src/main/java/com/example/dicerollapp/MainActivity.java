769ba81bccb4154d03ca160324921078a0095f74
Bitan701 <bitan701@gmail.com>
2021-01-01 20:49:53 +0600

    Sprint 3

--- Code/app/src/main/java/com/example/dicerollapp/MainActivity.java
+++ Code/app/src/main/java/com/example/dicerollapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textResult = findViewById(R.id.textResult);
         Button buttonRoll = findViewById(R.id.buttonRoll);
+        ImageView imageDice = findViewById(R.id.imageDice);
 
 
         buttonRoll.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +30,36 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 randomNumber = random.nextInt(5)+1;
                 textResult.setText(""+randomNumber);
+                if(randomNumber == 1)
+                {
+                    imageDice.setImageResource(R.mipmap.rolled_one);
+                    imageDice.setVisibility(View.VISIBLE);
+                }
+                if(randomNumber == 2)
+                {
+                    imageDice.setImageResource(R.mipmap.rolled_two);
+                    imageDice.setVisibility(View.VISIBLE);
+                }
+                if(randomNumber == 3)
+                {
+                    imageDice.setImageResource(R.mipmap.rolled_three);
+                    imageDice.setVisibility(View.VISIBLE);
+                }
+                if(randomNumber == 4)
+                {
+                    imageDice.setImageResource(R.mipmap.rolled_four);
+                    imageDice.setVisibility(View.VISIBLE);
+                }
+                if(randomNumber == 5)
+                {
+                    imageDice.setImageResource(R.mipmap.rolled_five);
+                    imageDice.setVisibility(View.VISIBLE);
+                }
+                if(randomNumber == 6)
+                {
+                    imageDice.setImageResource(R.mipmap.rolled_six);
+                    imageDice.setVisibility(View.VISIBLE);
+                }
             }
         });
     }
